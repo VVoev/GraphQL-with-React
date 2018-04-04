ff4e5b530d75b969b0a7854c99c86cab4f7fbecf
Vladimir Yonkov Voev <Vladimir.Voev@allocatesoftware.com>
2018-04-04 11:45:31 +0300

    finished schema

--- users/server.js
+++ users/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
-const port = 4000;
 
-app.use('/graphql',expressGraphQL({
-    graphiql: true
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
 }));
 
+
+const port = 4000;
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
 });
