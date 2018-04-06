5b8fd532e0e0e0338db09f0ab74cbd43f1e7ddaa
vladovoev <vladovoev@yahoo.co.uk>
2018-04-06 17:33:08 +0300

    Added optimisticResponse for faster rendering on UI

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { optimistic } from 'apollo-client/optimistic-data/store';
 
 class LyricList extends Component {
 
-    onLike(id) {
-        this.props.mutate({ variables: { id } })
+    onLike(id, likes) {
+        this.props.mutate({
+            variables: { id },
+            optimisticResponse: {
+                __typename: 'Mutation',
+                likeLyric: {
+                    id: id,
+                    __typename: 'LyricType',
+                    likes: likes + 1
+                }
+            }
+        })
     }
     renderLyrics() {
         return this.props.lyrics.map(({ id, content, likes }) => {
@@ -13,7 +24,7 @@ class LyricList extends Component {
                 <li key={id} className="collection-item">
                     {content}
                     <div className="vote-box">
-                        <i onClick={() => this.onLike(id)} className="material-icons">thumb_up</i>
+                        <i onClick={() => this.onLike(id, likes)} className="material-icons">thumb_up</i>
                         {likes}
                     </div>
                 </li>
