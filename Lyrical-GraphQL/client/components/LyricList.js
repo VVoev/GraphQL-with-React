a660f36d4d5e06f26cb416ec3140587cc73ad040
vladovoev <vladovoev@yahoo.co.uk>
2018-04-06 16:27:58 +0300

    thumb up

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 
 class LyricList extends Component {
 
+    onLike(id) {
+        console.log(id);
+    }
     renderLyrics() {
         return this.props.lyrics.map(({ id, content }) => {
             return (
                 <li key={id} className="collection-item">
                     {content}
+                    <i onClick={() => this.onLike(id)} className="material-icons">thumb_up</i>
                 </li>
             )
         })
