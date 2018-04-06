622a04410219f3be92abd7f211b9ae1e82c7c575
vladovoev <vladovoev@yahoo.co.uk>
2018-04-06 19:49:58 +0300

    integrated schema and mutation added nodemon

--- auth-graphql-starter/index.js
+++ auth-graphql-starter/index.js
@@ -1,5 +1,5 @@
 const app = require('./server/server');
 const port = 4000;
 app.listen(port, () => {
-  console.log('Listening');
+  console.log(`Server started and Listening on Port:${port}`);
 });
