b455e30660298c515c2fde2ef680a1ec1de9adac
vladovoev <vladovoev@yahoo.co.uk>
2018-04-06 12:46:41 +0300

    fetching a song from a list of songs thru ReactRouter = > graphql = > SongDetails

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,15 +1,16 @@
+import styles from './style/style.css'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
 import App from './components/App'
 import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
-
-import styles from './style/style.css'
-
+import SongDetail from './components/SongDetail';
 
 const client = new ApolloClient({});
 
@@ -20,6 +21,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
           <Route path="/songs/new" component={SongCreate} />
+          <Route path="/songs/:id" component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
